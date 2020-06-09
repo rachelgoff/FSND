54d928001ced5d572edb1803567a0656700ee558
Rachel <rzhangmoz1@gmail.com>
2020-06-08 20:36:42 -0700

    update

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../backend/src')))
+print(os.path.abspath(os.path.join(os.path.dirname(__file__), '../backend/src')))
+print("test")
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app
-from models import db
+from .database.models import db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
